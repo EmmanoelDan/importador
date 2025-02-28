5d33fa75ab113951a17e4341d2430eb7ab6c44da
EmmanoelDan <emmanuel2011.07@gmail.com>
2025-02-28 19:24:46 -0300

    fix: correcting subscription model

--- model/subscription.go
+++ model/subscription.go
@@ -1,8 +1,8 @@
 package model
 
 type Subscription struct {
-	SubscriptionId        string `gorm:"primaryKey;column:subscriptionid"`
-	CustomerId            string `gorm:"column:customerid"`
-	ProductId             string `gorm:"column:productid"`
-	SubscriptionStartDate string `gorm:"column:subscriptionstartdate"`
+	SubscriptionId          string `gorm:"primaryKey;column:subscriptionid"`
+	CustomerId              string `gorm:"column:customerid"`
+	ProductId               string `gorm:"column:productid"`
+	SubscriptionDescription string `gorm:"column:subscriptiondescription"`
 }
