71ca697338a61b4d197dc7e6fe052ec9385cf7ce
EmmanoelDan <emmanuel2011.07@gmail.com>
2025-02-28 19:23:54 -0300

    fix: correcting billing model

--- model/billing.go
+++ model/billing.go
@@ -1,36 +1,34 @@
 package model
 
-import (
-	"gorm.io/datatypes"
-)
+import "gorm.io/datatypes"
 
 type Billing struct {
-	BillingId              string         `gorm:"primaryKey;column:billingid"`
-	PartnerId              string         `gorm:"column:partnerid"`
-	SubscriptionId         string         `gorm:"column:subscriptionid"`
-	MeterId                string         `gorm:"column:meterid"`
-	EntitlementId          string         `gorm:"column:etitlementid"`
-	InvoiceNumber          string         `gorm:"column:invoicenumber"`
-	ChargeStartDate        string         `gorm:"column:chargestartdate"`
-	ChargeEndDate          string         `gorm:"column:chargesenddate"`
-	UsageDate              string         `gorm:"column:usagedate"`
-	ResourceLocation       string         `gorm:"column:resourcelocation"`
-	ConsumedService        string         `gorm:"column:consumedservice"`
-	ResouceGroup           string         `gorm:"column:resoucegroup"`
-	ResouceURI             string         `gorm:"column:resouceuri"`
-	ChargeType             string         `gorm:"column:chargetype"`
-	UnitPrice              float64        `gorm:"column:unitprice"`
-	Quantity               float64        `gorm:"column:quantity"`
-	UnitType               string         `gorm:"column:unittype"`
-	BillingPreTaxTotal     float64        `gorm:"column:billingpretax_total"`
-	BillingCurrency        string         `gorm:"column:billingcurrency"`
-	PricingPreTaxTotal     string         `gorm:"column:pricingpretax_total"`
-	PricingCurrency        string         `gorm:"column:pricingcurrency"`
-	ServiceInfo1           string         `gorm:"column:serviceinfo1"`
-	ServiceInfo2           string         `gorm:"column:serviceinfo2"`
-	Tags                   datatypes.JSON `gorm:"column:tags;type:json"`
+	BillingId              string          `gorm:"primaryKey;column:billingid;autoIncrement"`
+	PartnerId              string          `gorm:"column:partnerid"`
+	SubscriptionId         string          `gorm:"column:subscriptionid"`
+	MeterId                string          `gorm:"column:meterid"`
+	EntitlementId          string          `gorm:"column:entitlementid"`
+	InvoiceNumber          string          `gorm:"column:invoicenumber"`
+	ChargeStartDate        string          `gorm:"column:chargestartdate"`
+	ChargeEndDate          string          `gorm:"column:chargeenddate"`
+	UsageDate              string          `gorm:"column:usagedate"`
+	ResourceLocation       string          `gorm:"column:resourcelocation"`
+	ConsumedService        string          `gorm:"column:consumedservice"`
+	ResourceGroup          string          `gorm:"column:resourcegroup"`
+	ResourceURI            string          `gorm:"column:resourceuri"`
+	ChargeType             string          `gorm:"column:chargetype"`
+	UnitPrice              string          `gorm:"column:unitprice"`
+	Quantity               string          `gorm:"column:quantity"`
+	UnitType               string          `gorm:"column:unittype"`
+	BillingPreTaxTotal     string          `gorm:"column:billingpretaxtotal"`
+	BillingCurrency        string          `gorm:"column:billingcurrency"`
+	PricingPreTaxTotal     string          `gorm:"column:pricingpretaxtotal"`
+	PricingCurrency        string          `gorm:"column:pricingcurrency"`
+	ServiceInfo1           string          `gorm:"column:serviceinfo1"`
+	ServiceInfo2           string          `gorm:"column:serviceinfo2"`
+	Tags                   datatypes.JSON      `gorm:"column:tags;type:json"`
 	AdditionalInfo         datatypes.JSON `gorm:"column:additionalinfo;type:json"`
-	EffectiveUnitPrice     float64        `gorm:"column:effectiveunitprice"`
-	PCToBCExchangeRate     int16          `gorm:"column:pctobcexchangerate"`
-	PCToBCExchangeRateDate string         `gorm:"column:pctobcexchangeratedate"`
+	EffectiveUnitPrice     string          `gorm:"column:effectiveunitprice"`
+	PCToBCExchangeRate     string          `gorm:"column:pctobcexchangerate"`
+	PCToBCExchangeRateDate string          `gorm:"column:pctobcexchangeratedate"`
 }
