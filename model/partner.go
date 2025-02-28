3c472601af043102884d7674d63e83ebcb6850eb
EmmanoelDan <emmanuel2011.07@gmail.com>
2025-02-28 19:24:37 -0300

    fix: correcting partner model

--- model/partner.go
+++ model/partner.go
@@ -3,4 +3,6 @@ package model
 type Partner struct {
 	PartnerId   string `gorm:"primaryKey;column:partnerid"`
 	PartnerName string `gorm:"column:partnername"`
+	MpnId       string `gorm:"column:mpnid"`
+	Tier2MpnId  string `gorm:"column:tier2mpnid"`
 }
