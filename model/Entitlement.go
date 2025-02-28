19297a22ad04a0ec3dbe3ca22bdd6632da0fd8d7
EmmanoelDan <emmanuel2011.07@gmail.com>
2025-02-28 19:24:27 -0300

    fix: correcting entitlement model

--- model/Entitlement.go
+++ model/Entitlement.go
@@ -1,12 +1,12 @@
 package model
 
 type Entitlement struct {
-	EntitlementId                 string `gorm:"primaryKey"`
+	EntitlementId                 string `gorm:"primaryKey;column:entitlementid"`
 	EntitlementDescription        string `gorm:"column:entitlementdescription"`
-	PartnerEarnedCreditPercentage int16  `gorm:"column:partnerEarnedCreditPercentage"`
-	CreditPercentage              int16  `gorm:"column:creditPercentage"`
-	CreditType                    string `gorm:"column:creditType"`
-	BenefitOrderId                string `gorm:"column:benefitorder"`
+	PartnerEarnedCreditPercentage string `gorm:"column:partnerearnedcreditpercentage"`
+	CreditPercentage              string `gorm:"column:creditpercentage"`
+	CreditType                    string `gorm:"column:credittype"`
+	BenefitOrderId                string `gorm:"column:benefitorderid"`
 	BenefitId                     string `gorm:"column:benefitid"`
 	BenefitType                   string `gorm:"column:benefittype"`
 }
